a75e2bdca39e259f748b61efaab74084a112f814
Xeryto <igoshindanya@mail.ru>
2022-09-24 00:09:19 +0300

    Add base logic

---
+++ config.py
@@ -0,0 +1,2 @@
+BOT_TOKEN = "5430424349:AAE8BVZQsdST-t9zstNWycSmYQ3lQmtQ0Ow"
+APP_URL = "https://telebot-ibiblee.herokuapp.com/" + BOT_TOKEN
--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import telebot
 import psycopg2
+import os
+import logging
 from telebot import types
+from config import *
+from flask import Flask, request
+
 
 bot = telebot.TeleBot("5430424349:AAE8BVZQsdST-t9zstNWycSmYQ3lQmtQ0Ow")
-
+server = Flask(__name__)
+logger = telebot.logger
+logger.setLevel(logging.DEBUG)
 conn = psycopg2.connect(
     host="localhost",
-    database="goldp1",
-    port="5433")
+    database="postgres",
+    port="5432",
+    user="postgres"
+)
 cursor = conn.cursor()
+
+@server.route(f"/{BOT_TOKEN}", methods=["POST"])
+def redirect_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_messages([update])
+    return "!", 200
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    server.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
 
 def insert_part(part: int, user_id: int, username: str):
     cursor.execute('INSERT INTO user_part (user_id, part, username) VALUES (%s, %s, %s)', (user_id, part, username))
