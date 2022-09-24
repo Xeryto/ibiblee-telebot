363efb1822415c6a33a7e6836909937cb703bb93
Xeryto <igoshindanya@mail.ru>
2022-09-24 11:55:34 +0300

    fixed message redirect

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ cursor = conn.cursor()
 
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
 def redirect_message():
-    json_string = request.get_data().decode('utf-8')
+    json_string = request.get_data().decode("utf-8")
     update = telebot.types.Update.de_json(json_string)
-    bot.process_new_messages([update])
+    bot.process_new_updates([update])
     return "!", 200
 
 if __name__ == "__main__":
