b65234d601866fa2070981b8f7f8f446d58da7a9
Xeryto <igoshindanya@mail.ru>
2022-09-24 11:46:00 +0300

    Add database conn

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 BOT_TOKEN = "5430424349:AAE8BVZQsdST-t9zstNWycSmYQ3lQmtQ0Ow"
 APP_URL = "https://telebot-ibiblee.herokuapp.com/" + BOT_TOKEN
+DB_URL = "postgres://ezhmwxqcljauvf:6b0fb6a8f78199c445399139328b7a540d64ab29d2a2201311bbb9d6a1be9cca@ec2-54-170-90-26.eu-west-1.compute.amazonaws.com:5432/db949b7a4di5ov"
