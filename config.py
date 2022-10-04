2ad13f3522acdf8fc55c42f1bf90ae228c71c4bd
Xeryto <igoshindanya@mail.ru>
2022-10-04 13:55:48 +0300

    changed db url

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "5430424349:AAE8BVZQsdST-t9zstNWycSmYQ3lQmtQ0Ow"
 APP_URL = "https://telebot-ibiblee.herokuapp.com/" + BOT_TOKEN
-DB_URL = "postgres://ezhmwxqcljauvf:6b0fb6a8f78199c445399139328b7a540d64ab29d2a2201311bbb9d6a1be9cca@ec2-54-170-90-26.eu-west-1.compute.amazonaws.com:5432/db949b7a4di5ov"
+DB_URL = "postgres://ehigvggwxdkhzc:9719a9c2cfcf2b247785a2b31cbad60614b4e5a0e88187ed4f86e9b1052e3390@ec2-34-246-25-222.eu-west-1.compute.amazonaws.com:5432/d1cbaqmdr0qqsi"
