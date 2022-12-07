16a8f773b46a7f575463ce24252d7f6b2a9165de
Xeryto <igoshindanya@mail.ru>
2022-12-07 23:30:20 +0300

    db url fix

--- api/config.py
+++ api/config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "5430424349:AAE8BVZQsdST-t9zstNWycSmYQ3lQmtQ0Ow"
 APP_URL = "https://ibiblee-telebot.vercel.app/api/" + BOT_TOKEN
-DB_URL = "postgresql://mouse.db.elephantsql.com:5432/bchrsgcm"
+DB_URL = "postgres://bchrsgcm:RCnenUulaYxJhM-FZvqDh8TEhHE-UmGS@mouse.db.elephantsql.com/bchrsgcm"
