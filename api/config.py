f1c99f0a75bfb7f71b09b22b280e6ca25d54bc95
Xeryto <igoshindanya@mail.ru>
2022-12-07 23:27:16 +0300

    switch from heroku to vercel

---
+++ api/config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "5430424349:AAE8BVZQsdST-t9zstNWycSmYQ3lQmtQ0Ow"
+APP_URL = "https://ibiblee-telebot.vercel.app/api/" + BOT_TOKEN
+DB_URL = "postgresql://mouse.db.elephantsql.com:5432/bchrsgcm"
--- config.py
+++
@@ -1,3 +0,0 @@
-BOT_TOKEN = "5430424349:AAE8BVZQsdST-t9zstNWycSmYQ3lQmtQ0Ow"
-APP_URL = "https://telebot-ibiblee.herokuapp.com/" + BOT_TOKEN
-DB_URL = "postgres://ehigvggwxdkhzc:9719a9c2cfcf2b247785a2b31cbad60614b4e5a0e88187ed4f86e9b1052e3390@ec2-34-246-25-222.eu-west-1.compute.amazonaws.com:5432/d1cbaqmdr0qqsi"
